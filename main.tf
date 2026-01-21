3759fdd62e2fab625360180e7ad66c3009c20de9
Satyam Aditya <satyamaditya567.com>
2026-01-21 09:50:52 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ashrams"
+  location = "Central US"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "TerraformVNetashrams"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "TerraformSubnetashrams"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "TerraformNICCashrams"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "vmashrams"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = "central us"  # Ensure the region supports Zone 3
+  size                  = "Standard_D2s_v3"  # Use the available VM size in Zone 3
+  admin_username        = "Administrator1"
+  admin_password        = "Networklabs@1234"  # Replace with a secure password
+
+  disable_password_authentication = false  # Allow password authentication
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+
+}
